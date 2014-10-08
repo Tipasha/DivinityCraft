638de0968696502fe6e4b077c88b8bd3a44e2354
Pavel <teppasha@gmail.com>
2014-10-08 18:08:40 +0300

    rf recipe win

--- app/controllers/Recipes/SingleRecipeWin.js
+++ app/controllers/Recipes/SingleRecipeWin.js
@@ -1,40 +1,27 @@
+var ingMap = ["ing_1", "ing_2"];
+
+$.ing_1TblSections = [];
+$.ing_2TblSections = [];
+
 _.each($model.get("connections"), function(rec) {
-	var leftTblData = [];
-	var rightTblData = [];
+	_.each(ingMap, function(ing) {
+		$[ing + "TblSection"] = Ti.UI.createTableViewSection();
 
-	_.each(rec.ing_1, function(ing) {
-		var row = Ti.UI.createTableViewRow({
-			height : 44
+		_.each(rec[ing], function(ingredient) {
+			var row = Ti.UI.createTableViewRow({
+				height : 44,
+				title : ingredient.name
+			});
+
+			$[ing + "TblSection"].add(row);
 		});
-
-		var title = Ti.UI.createLabel({
-			left : 0,
-			text : ing.name
-		});
-
-		row.add(title);
-
-		leftTblData.push(row);
+		$[ing + "TblSections"].push($[ing + "TblSection"]);
 	});
-
-	_.each(rec.ing_2, function(ing) {
-		var row = Ti.UI.createTableViewRow({
-			height : 44
-		});
-
-		var title = Ti.UI.createLabel({
-			left : 0,
-			text : ing.name
-		});
-
-		row.add(title);
-
-		rightTblData.push(row);
-	});
-	$.leftTbl.data = leftTblData;
-	$.rightTbl.data = rightTblData;
-	addPlusView(44 * 3);
+	addPlusView(44 * (rec.ing_1.length > rec.ing_2.length ? rec.ing_1.length : rec.ing_2.length));
 });
+
+$.leftTbl.sections = $.ing_1TblSections;
+$.rightTbl.sections = $.ing_2TblSections;
 
 function addPlusView(height) {
 	var self = Ti.UI.createView({
