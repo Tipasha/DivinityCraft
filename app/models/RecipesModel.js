f315bcb1301edb2c3fa843c4c76b83f5c61a6db1
Pavel <teppasha@gmail.com>
2014-10-20 19:08:37 +0300

    add recipe model

--- app/models/RecipesModel.js
+++ app/models/RecipesModel.js
@@ -1,6 +1,5 @@
 exports.definition = {
 	config: {
-
 		adapter: {
 			type: "properties",
 			collection_name: "RecipesModel"
@@ -15,7 +14,31 @@ exports.definition = {
 	},
 	extendCollection: function(Collection) {
 		_.extend(Collection.prototype, {
-			// extended functions and properties go here
+			initialize : function() {
+				this.prefix = Alloy.CFG.queryURL + "feed/_all_docs?include_docs=true";
+			},
+			loadMore : function() {
+				var self = this;
+				Alloy.Globals.AJAX.getJSON(self.prefix, false, function(json) {
+					if (json && json.status == 200) {
+						var result = json.result.rows;
+						self.add(result);
+					} else {
+						self.trigger("error_loading");
+					}
+				});
+			},
+			reload : function() {
+				var self = this;
+				Alloy.Globals.AJAX.getJSON(self.prefix, false, function(json) {
+					if (json && json.status == 200) {
+						var result = json.result.rows;
+						self.reset(result);
+					} else {
+						self.trigger("error_loading");
+					}
+				});
+			}
 		});
 
 		return Collection;
